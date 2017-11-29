a5c569eb346f1358748197ab38542bfd191ac985
kamakuni <superradio86@gmail.com>
2017-11-30 01:00:37 +0900

    add copyn implementation

---
+++ reader/copyn/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+func copyN(dest io.Writer, src io.Reader, length int) (n int64, err error) {
+	written, err := io.Copy(dest, &io.LimitedReader{src, n})
+	if err != nil {
+		return 0, err
+	}
+	return written, nil
+}
+
+func main() {
+	reader := strings.NewReader("1234567890")
+	copyN(os.Stdout, reader, 5)
+}
