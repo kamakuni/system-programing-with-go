1e3934ed2cedab559c39580ab80cd6f5757f8e4e
kamakuni <superradio86@gmail.com>
2018-01-17 23:30:37 +0900

    add pty tutorial

---
+++ pty/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/kr/pty"
+	"io"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("./check/check")
+	stdpty, stdtty, _ := pty.Open()
+	defer stdtty.Close()
+	cmd.Stdin = stdpty
+	cmd.Stdout = stdpty
+	errpty, errtty, _ := pty.Open()
+	defer errtty.Close()
+	cmd.Stderr = errtty
+	go func() {
+		io.Copy(os.Stdout, stdpty)
+	}()
+	go func() {
+		io.Copy(os.Stderr, errpty)
+	}()
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
