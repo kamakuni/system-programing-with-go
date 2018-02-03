84d4dafac000fbc3eaad330f9c5630498e063f76
kamakuni <superradio86@gmail.com>
2018-02-04 00:02:33 +0900

    add mutex tutorial

---
+++ concurrency/mutex/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var id int
+
+func generateID(mutex *sync.Mutex) int {
+	mutex.Lock()
+	defer mutex.Unlock()
+	id++
+	return id
+}
+
+func main() {
+
+	var mutex sync.Mutex
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			fmt.Printf("id: %d\n", generateID(&mutex))
+		}()
+	}
+}
