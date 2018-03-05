64dac3157f8eb1b382434c23cae7a9b1f99b9bee
kamakuni <superradio86@gmail.com>
2018-03-05 23:51:42 +0900

    add empty main file

---
+++ container/main.go
@@ -0,0 +1,22 @@
+package main
+
+package main
+ 
+import (
+    "github.com/opencontainers/runc/libcontainer"
+    "github.com/opencontainers/runc/libcontainer/configs"
+    _ "github.com/opencontainers/runc/libcontainer/nsenter"
+    "log"
+    "os"
+    "runtime"
+    "path/filepath"
+    "golang.org/x/sys/unix"
+)
+
+func init(){
+
+}
+
+func main(){
+	
+}
